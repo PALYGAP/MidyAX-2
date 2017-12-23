cc73147fd2ab640f0b80cf4a36bd7d7ce9f11e90
PALYGAP <eric.feuilleaubois@sfr.fr>
2017-12-23 18:28:31 +0100

    Added license & infos
    Added license & infos

--- parammapping.cpp
+++ parammapping.cpp
@@ -1,3 +1,14 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// PROGRAM:     MidyAX 2 - MIDI communications orchestrator - MIDI controllers to AXE-FX           //
+// AIM:         Provide enhanced usability of the AXE-FX by making it possible to set the          //
+//              AXE-FX parameters with a hardware interface (knobs/switches of a MIDI controller   //
+//              like : BCR2000 /BCF2000, XTouch Mini, BeatStep, Guitar Wing ... )                  //
+// CREATOR:     Eric FEUILLEAUBOIS                                                                 //
+// PROG ENV:    Qt v5.6 or above, RTMIDI library (https://github.com/thestk/rtmidi)                //
+// HARDWARE:    Tested on RPi / PC / MAC / Linux Box / Pipo X8                                     //
+// LICENSE:     GNU GPL v3 - That means OPEN SOFWARE, COPYLEFT and hope it's useful to you         //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include "parammapping.h"
 #include "ui_parammapping.h"
 
